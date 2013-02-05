11b12e0e65a9d8286232f8889ab862025b3578c9
Arvid Böttiger <bottiger@gmail.com>
2013-02-05 11:43:58 +0100

    Added: WelcomeActivity which will be used for setting up the app on first run

---
+++ src/info/bottiger/podcast/WelcomeActivity.java
@@ -0,0 +1,87 @@
+package info.bottiger.podcast;
+
+import info.bottiger.podcast.adapters.TestFragmentAdapter;
+
+import java.util.Random;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.viewpagerindicator.CirclePageIndicator;
+import com.viewpagerindicator.PageIndicator;
+
+public class WelcomeActivity extends FragmentActivity {
+	
+    private static final Random RANDOM = new Random();
+
+    TestFragmentAdapter mAdapter;
+    ViewPager mPager;
+    PageIndicator mIndicator;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_swipe, menu);
+        return true;
+    }
+
+    /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.random:
+                final int page = RANDOM.nextInt(mAdapter.getCount());
+                Toast.makeText(this, "Changing to page " + page, Toast.LENGTH_SHORT);
+                mPager.setCurrentItem(page);
+                return true;
+
+            case R.id.add_page:
+                if (mAdapter.getCount() < 10) {
+                    mAdapter.setCount(mAdapter.getCount() + 1);
+                    mIndicator.notifyDataSetChanged();
+                }
+                return true;
+
+            case R.id.remove_page:
+                if (mAdapter.getCount() > 1) {
+                    mAdapter.setCount(mAdapter.getCount() - 1);
+                    mIndicator.notifyDataSetChanged();
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_circles);
+
+        mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+
+        //We set this on the indicator, NOT the pager
+        mIndicator.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(WelcomeActivity.this, "Changed to page " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+}
---
+++ src/info/bottiger/podcast/adapters/TestFragmentAdapter.java
@@ -0,0 +1,52 @@
+package info.bottiger.podcast.adapters;
+
+import info.bottiger.podcast.R;
+import info.bottiger.podcast.TestFragment;
+import info.bottiger.podcast.R.drawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.viewpagerindicator.IconPagerAdapter;
+
+public class TestFragmentAdapter extends FragmentPagerAdapter implements IconPagerAdapter {
+    protected static final String[] CONTENT = new String[] { "This", "Is", "A", "Test", };
+    
+    protected static final int[] ICONS = new int[] {
+            R.drawable.av_play
+    };
+
+    private int mCount = CONTENT.length;
+
+    public TestFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return TestFragment.newInstance(CONTENT[position % CONTENT.length]);
+    }
+
+    @Override
+    public int getCount() {
+        return mCount;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+      return TestFragmentAdapter.CONTENT[position % CONTENT.length];
+    }
+
+    @Override
+    public int getIconResId(int index) {
+      return ICONS[index % ICONS.length];
+    }
+    
+
+    public void setCount(int count) {
+        if (count > 0 && count <= 10) {
+            mCount = count;
+            notifyDataSetChanged();
+        }
+    }
+}
