0d10e947de616ecbe2f57fefb4cea20edc578373
Arvid Böttiger <bottiger@gmail.com>
2015-02-10 23:53:33 +0100

    Added some debug logging

--- app/src/main/java/org/bottiger/podcast/receiver/HeadsetReceiver.java
+++ app/src/main/java/org/bottiger/podcast/receiver/HeadsetReceiver.java
@@ -10,39 +10,46 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.util.Log;
 import android.view.KeyEvent;
 
 public class HeadsetReceiver extends BroadcastReceiver {
 
-	private static List eventLog;
-	private PlayerService mPlayerServiceBinder = PodcastBaseFragment.mPlayerServiceBinder;
+    private static List eventLog;
+    private PlayerService mPlayerServiceBinder = PodcastBaseFragment.mPlayerServiceBinder;
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		// if (Intent.ACTION_HEADSET_PLUG.equals(intent.getAction())) {
-		if (AudioManager.ACTION_AUDIO_BECOMING_NOISY.equals(intent.getAction())) {
-			mPlayerServiceBinder = PodcastBaseFragment.mPlayerServiceBinder;
-			if (mPlayerServiceBinder != null) {
-				if (mPlayerServiceBinder.isPlaying())
-					mPlayerServiceBinder.pause();
-			}
-		}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("HeadsetReceiver", "Action: " + intent.getAction().toString() + "");
 
-		if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
-			KeyEvent event = (KeyEvent) intent
-					.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+        if (AudioManager.ACTION_AUDIO_BECOMING_NOISY.equals(intent.getAction())) {
+            mPlayerServiceBinder = PodcastBaseFragment.mPlayerServiceBinder;
+            if (mPlayerServiceBinder != null) {
+                if (mPlayerServiceBinder.isPlaying())
+                    Log.d("HeadsetReceiver", "ACTION_AUDIO_BECOMING_NOISY => Pause Player");
+                mPlayerServiceBinder.pause();
+            }
+        }
+
+        if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
+            KeyEvent event = (KeyEvent) intent
+                    .getParcelableExtra(Intent.EXTRA_KEY_EVENT);
 
             if (event == null)
                 return;
 
-			if (KeyEvent.KEYCODE_HEADSETHOOK == event.getKeyCode()) {
-				if (KeyEvent.ACTION_DOWN == event.getAction()) {
-					if (mPlayerServiceBinder.isPlaying())
-						mPlayerServiceBinder.pause();
-					else
-						mPlayerServiceBinder.start();
-				}
-			}
-		}
-	}
+            Log.d("HeadsetReceiver", "Action: " + intent.getAction().toString() + " KeyCode: " + event.getKeyCode());
+            if (KeyEvent.KEYCODE_HEADSETHOOK == event.getKeyCode()) {
+                if (KeyEvent.ACTION_DOWN == event.getAction()) {
+                    if (mPlayerServiceBinder.isPlaying()) {
+                        Log.d("HeadsetReceiver", "ACTION_DOWN => Pause Player");
+                        mPlayerServiceBinder.pause();
+                    } else {
+                        Log.d("HeadsetReceiver", "ACTION_DOWN => Start Player");
+                        mPlayerServiceBinder.start();
+                    }
+                }
+            }
+        }
+    }
 }
