2ef1cfcff49916cb72f8acc4f088e762dd09a973
Arvid Böttiger <bottiger@gmail.com>
2015-02-26 12:39:14 +0100

    Minor cleanup

--- app/src/amazon/java/org/bottiger/podcast/flavors/CrashReporter/VendorCrashReporter.java
+++ app/src/amazon/java/org/bottiger/podcast/flavors/CrashReporter/VendorCrashReporter.java
@@ -1,13 +1,9 @@
 package org.bottiger.podcast.flavors.CrashReporter;
 
-import android.app.Activity;
 import android.app.Application;
-import android.content.Context;
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import org.acra.ACRA;
-import org.bottiger.podcast.SoundWaves;
 
 /**
  * Created by apl on 26-02-2015.
