1df45b28968bcf6187d6e7a99d879111c11dc794
Arvid Böttiger <bottiger@gmail.com>
2015-04-15 01:53:03 +0200

    Added more stub implementations to the descovery fragment

---
+++ app/src/main/java/org/bottiger/podcast/adapters/DiscoverySearchAdapter.java
@@ -0,0 +1,25 @@
+package org.bottiger.podcast.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by apl on 15-04-2015.
+ */
+public class DiscoverySearchAdapter<SearchResultViewHolder extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<SearchResultViewHolder> {
+
+    @Override
+    public SearchResultViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(SearchResultViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/org/bottiger/podcast/adapters/viewholders/discovery/SearchResultViewHolder.java
@@ -0,0 +1,14 @@
+package org.bottiger.podcast.adapters.viewholders.discovery;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by apl on 15-04-2015.
+ */
+public class SearchResultViewHolder extends RecyclerView.ViewHolder {
+
+    public SearchResultViewHolder(View itemView) {
+        super(itemView);
+    }
+}
