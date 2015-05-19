8e88139064a5f63b056ac5b5d981c8edd931f515
Arvid Böttiger <bottiger@gmail.com>
2015-05-19 21:17:42 +0200

    updated the subscription switch to material design on kitkat

--- app/src/main/java/org/bottiger/podcast/adapters/viewholders/discovery/SearchResultViewHolder.java
+++ app/src/main/java/org/bottiger/podcast/adapters/viewholders/discovery/SearchResultViewHolder.java
@@ -16,14 +16,14 @@ public class SearchResultViewHolder extends RecyclerView.ViewHolder {
 
     public TextView title;
     public SimpleDraweeView image;
-    public Switch toggleSwitch;
+    public com.rey.material.widget.Switch toggleSwitch;
 
     public SearchResultViewHolder(View itemView) {
         super(itemView);
 
         title = (TextView) itemView.findViewById(R.id.result_title);
         image = (SimpleDraweeView) itemView.findViewById(R.id.result_image);
-        toggleSwitch = (Switch) itemView.findViewById(R.id.result_subscribe_switch);
+        toggleSwitch = (com.rey.material.widget.Switch) itemView.findViewById(R.id.result_subscribe_switch);
 
     }
 }
