ed0f676f8371db6c34e20f39f2ac6bd39a2a8b70
Arvid Böttiger <bottiger@gmail.com>
2015-05-24 17:55:04 +0200

    Removed ReativeayoutWithBackground

--- app/src/main/java/org/bottiger/podcast/views/SwipeRefreshExpandableLayout.java
+++ app/src/main/java/org/bottiger/podcast/views/SwipeRefreshExpandableLayout.java
@@ -15,9 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.OverScroller;
 
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
-
 import org.bottiger.podcast.PlaylistFragment;
 import org.bottiger.podcast.R;
 
