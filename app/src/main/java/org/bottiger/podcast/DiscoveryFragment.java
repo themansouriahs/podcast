205763ef43d4aa7e0cc154cde2770122d3619773
Arvid Böttiger <bottiger@gmail.com>
2016-06-09 15:27:15 +0200

    Prevent the Keyboard from popping up in the Discovery Fragment

--- app/src/main/java/org/bottiger/podcast/DiscoveryFragment.java
+++ app/src/main/java/org/bottiger/podcast/DiscoveryFragment.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
