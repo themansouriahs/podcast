ed2f7843989dc0c52acf1f8e82a820f53d181f2c
Arvid Böttiger <bottiger@gmail.com>
2016-06-16 09:26:24 +0200

    Added infrastructure for silence removal. Still hardcoded to off.

--- app/src/main/java/org/bottiger/podcast/Player/exoplayer/PodcastAudioRendererV21.java
+++ app/src/main/java/org/bottiger/podcast/Player/exoplayer/PodcastAudioRendererV21.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.media.MediaCodec;
 import android.media.MediaFormat;
 import android.os.Build;
+import android.util.Log;
 
 import com.google.android.exoplayer.ExoPlaybackException;
 import com.google.android.exoplayer.SampleSource;
@@ -19,15 +20,23 @@ import java.nio.ByteBuffer;
  */
 public class PodcastAudioRendererV21 extends PodcastAudioRenderer {
 
+    private static final String TAG = "PodcastAudioRendererV21";
+
     private static final int SAMPLES_PER_CODEC_FRAME = 1_024;
 
     private Sonic mSonic;
     private byte[] mSonicInputBuffer;
     private byte[] mSonicOutputBuffer;
 
+    private SilenceRemover mSilenceRemover;
+
     private float mSpeed = PlaybackSpeed.DEFAULT;
+    private boolean mRemoveSilence = false;
+
     private int mLastSeenBufferIndex = -1;
     private ByteBuffer mLastInternalBuffer;
+    private byte[] mSilenceInputBuffer;
+    private byte[] mSilenceOutputBuffer;
 
     public PodcastAudioRendererV21(SampleSource source) {
         super(source);
@@ -36,6 +45,10 @@ public class PodcastAudioRendererV21 extends PodcastAudioRenderer {
     public synchronized void setSpeed(float speed) {
         this.mSpeed = speed;
         this.mSonic.setSpeed(speed);
+    }
+
+    public synchronized void setRemoveSilence(boolean argRemoveSilence) {
+        this.mRemoveSilence = argRemoveSilence;
     }
 
     @Override
@@ -49,7 +62,8 @@ public class PodcastAudioRendererV21 extends PodcastAudioRenderer {
         } else {
             mLastSeenBufferIndex = bufferIndex;
         }
-        final int bytesToRead;
+
+        int bytesToRead;
 
         if (Util.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             buffer.position(0);
@@ -59,7 +73,28 @@ public class PodcastAudioRendererV21 extends PodcastAudioRenderer {
         }
 
         buffer.get(mSonicInputBuffer, 0, bytesToRead);
-        mSonic.writeBytesToStream(mSonicInputBuffer, bytesToRead);
+
+        int bytesOld = bytesToRead;
+
+        // Remove Silence
+        if (mRemoveSilence) {
+            mSilenceOutputBuffer = mSilenceRemover.removeSilence(mSonicInputBuffer, mSilenceOutputBuffer, bytesToRead);
+            bytesToRead = mSilenceRemover.getOutputLength();
+        } else {
+            mSilenceOutputBuffer = mSonicInputBuffer;
+        }
+
+        long us_skipped = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            if (bufferInfo.size != 0) {
+                long us_pr_frame = (bufferInfo.presentationTimeUs / bufferInfo.size);
+                us_skipped = us_pr_frame * (bytesOld - bytesToRead);
+            }
+        }
+        Log.w(TAG, "bytediff: " + (bytesOld-bytesToRead) + "time skipped: " + us_skipped);
+        positionUs += us_skipped;
+
+        mSonic.writeBytesToStream(mSilenceOutputBuffer, bytesToRead);
         final int readThisTime = mSonic.readBytesFromStream(mSonicOutputBuffer, mSonicOutputBuffer.length);
 
         bufferInfo.offset = 0;
@@ -82,6 +117,10 @@ public class PodcastAudioRendererV21 extends PodcastAudioRenderer {
 
         // Two samples per frame * 2 to support narration speeds down to 0.5
         final int bufferSizeBytes = SAMPLES_PER_CODEC_FRAME * 2 * 2 * channelCount;
+
+        this.mSilenceInputBuffer = new byte[bufferSizeBytes];
+        this.mSilenceOutputBuffer = new byte[bufferSizeBytes];
+        mSilenceRemover = new SilenceRemover(sampleRate, channelCount);
 
         this.mSonicInputBuffer = new byte[bufferSizeBytes];
         this.mSonicOutputBuffer = new byte[bufferSizeBytes];
