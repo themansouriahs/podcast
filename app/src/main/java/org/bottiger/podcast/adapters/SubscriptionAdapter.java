9162bb1dc5ae62d95ec349be96295d204461ef2f
Arvid Böttiger <bottiger@gmail.com>
2016-12-14 13:44:54 +0100

    Cleaned up the SubscriptionAdapter. Better handling of subscription multi sleection

--- app/src/main/java/org/bottiger/podcast/adapters/SubscriptionAdapter.java
+++ app/src/main/java/org/bottiger/podcast/adapters/SubscriptionAdapter.java
@@ -10,15 +10,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
 import android.support.v7.util.SortedList;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
 import android.util.Log;
-import android.util.Patterns;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,21 +28,17 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
 
 import org.bottiger.podcast.R;
-import org.bottiger.podcast.SoundWaves;
 import org.bottiger.podcast.ToolbarActivity;
 import org.bottiger.podcast.activities.feedview.FeedActivity;
-import org.bottiger.podcast.adapters.viewholders.FooterViewHolder;
 import org.bottiger.podcast.adapters.viewholders.subscription.AuthenticationViewHolder;
 import org.bottiger.podcast.adapters.viewholders.subscription.SubscriptionViewHolder;
-import org.bottiger.podcast.listeners.PaletteListener;
-import org.bottiger.podcast.playlist.Playlist;
+import org.bottiger.podcast.provider.ISubscription;
 import org.bottiger.podcast.provider.Subscription;
 import org.bottiger.podcast.provider.base.BaseSubscription;
 import org.bottiger.podcast.utils.ColorExtractor;
+import org.bottiger.podcast.utils.ErrorUtils;
 import org.bottiger.podcast.utils.ImageLoaderUtils;
-import org.bottiger.podcast.utils.PaletteHelper;
 import org.bottiger.podcast.utils.StrUtils;
-import org.bottiger.podcast.utils.UIUtils;
 
 import java.util.List;
 
@@ -70,61 +63,9 @@ public class SubscriptionAdapter extends RecyclerView.Adapter {
     private int numberOfColumns = 2;
     private int position = -1;
 
-    private ActionMode mActionMode = null;
+    @Nullable private ActionMode mActionMode = null;
     private MultiSelector mMultiSelector = new MultiSelector();
-    private ModalMultiSelectorCallback mActionModeCallback
-            = new ModalMultiSelectorCallback(mMultiSelector) {
-        @Override
-        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
-
-            List<Integer> positions = mMultiSelector.getSelectedPositions();
-
-            if (mSubscriptions == null)
-                return false;
-
-            switch (menuItem.getItemId()) {
-                case R.id.unsubscribe:
-                    actionMode.finish();
-
-                    for (int i = 0; i < positions.size(); i++) {
-                        int position = positions.get(i);
-                        Subscription subscription = mSubscriptions.get(position);
-
-                        if (subscription == null)
-                            return false;
-
-                        subscription.unsubscribe("Unsubscribe:context");
-                        notifyItemRemoved(position);
-                    }
-
-                    mMultiSelector.clearSelections();
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        @Override
-        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
-            super.onCreateActionMode(actionMode, menu);
-            mActivity.getMenuInflater().inflate(R.menu.subscription_context, menu);
-
-            return true;
-        }
-
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            boolean onPrepared = super.onPrepareActionMode(mode, menu);
-
-            String title = String.valueOf(mMultiSelector.getSelectedPositions().size() + 1 );
-            mode.setTitle(title);
-
-            return onPrepared;
-        }
-
-        public void onDestroyActionMode(ActionMode mode) {
-            super.onDestroyActionMode(mode);
-        }
-    };
+    private ModalMultiSelectorCallback mActionModeCallback = getModalMultiSelectorCallback();
 
     public SubscriptionAdapter(Activity argActivity, int argColumnsCount) {
         mActivity = argActivity;
@@ -137,18 +78,13 @@ public class SubscriptionAdapter extends RecyclerView.Adapter {
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Log.v(TAG, "onCreateViewHolder");
 
-        RecyclerView.ViewHolder holder = null;
+        RecyclerView.ViewHolder holder;
 
         switch (viewType)
         {
-            case FOOTER_TYPE: {
-                View view = mInflater.inflate(R.layout.recycler_item_empty_footer, parent, false);
-                holder = new FooterViewHolder(view);
-                break;
-            }
             case AUTHENTICATE_TYPE: {
                 View view = mInflater.inflate(R.layout.subscription_authenticate, parent, false);
-                holder = new AuthenticationViewHolder(view);
+                holder = new AuthenticationViewHolder(view, mMultiSelector);
                 break;
             }
             default: {
@@ -164,109 +100,98 @@ public class SubscriptionAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder argHolder, final int position) {
 
-        // In case we are dealing with the footer
-        if (isLastItem(position)) {
-            FooterViewHolder footer = (FooterViewHolder)argHolder;
-
-            android.support.v7.widget.GridLayoutManager.LayoutParams params = (android.support.v7.widget.GridLayoutManager.LayoutParams)footer.getFooter().getLayoutParams();
-            params.height = ToolbarActivity.getNavigationBarHeight(mActivity.getResources());
-            footer.getFooter().setLayoutParams(params);
-            footer.getFooter().requestLayout();
-
-            return;
-        }
-
-        Subscription sub = null;
+        Subscription subscription = null;
         try {
-            sub = mSubscriptions.get(position);
+            subscription = mSubscriptions.get(position);
         } catch (IllegalStateException e) {
-            e.printStackTrace();
+            ErrorUtils.handleException(e);
         }
 
-        if (null == sub) {
+        if (null == subscription) {
             return;
         }
 
-        if (getItemViewType(position) == AUTHENTICATE_TYPE) {
-            AuthenticationViewHolder holder = (AuthenticationViewHolder)argHolder;
-            holder.url = sub.getURLString();
-            return;
+        switch (getItemViewType(position)) {
+            case AUTHENTICATE_TYPE: {
+                onBindAuthenticationHolder((AuthenticationViewHolder) argHolder, subscription);
+            }
+            case GRID_TYPE:
+            case LIST_TYPE:{
+                onBindDefaultHolder((SubscriptionViewHolder)argHolder, subscription);
+            }
         }
+    }
 
-        final SubscriptionViewHolder holder = (SubscriptionViewHolder)argHolder;
+    private void onBindAuthenticationHolder(AuthenticationViewHolder argHolder, @NonNull final Subscription argSubscription) {
+        argHolder.url = argSubscription.getURLString();
+    }
 
-        final Subscription subscription = sub;
+    private void onBindDefaultHolder(@NonNull final SubscriptionViewHolder argHolder, @NonNull final Subscription argSubscription) {
 
+        final String logo = argSubscription.getImageURL();
+        final Resources resources = mActivity.getResources();
 
-        final String logo = sub.getImageURL();
-
-        if (subscription.getLastItemUpdated() > 0 && holder.subTitle != null) {
-            String reportDate = DateUtils.getRelativeTimeSpanString(subscription.getLastItemUpdated()).toString(); //df.format(date);
-
-            String updatedAt = mActivity.getResources().getString(R.string.subscription_subtitle_updated_at);
-            holder.subTitle.setText(String.format("%s %s", updatedAt, reportDate));
-        }
-
-
-        int newEpisodeCount = subscription.getNewEpisodes();
+        int newEpisodeCount = argSubscription.getNewEpisodes();
+        CharSequence pluralNew = getNewEpisodesString(newEpisodeCount);
         boolean hasNewEpisodes = newEpisodeCount > 0;
-        CharSequence pluralNew = "";
-        if (hasNewEpisodes) {
-            pluralNew = mActivity.getResources().getQuantityText(R.plurals.subscription_list_new, newEpisodeCount);
+        boolean hasImage = !TextUtils.isEmpty(logo);
+        int visibility = hasNewEpisodes || !hasImage ? View.VISIBLE : View.GONE;
+        Uri imageURI = hasImage ? Uri.parse(logo) : null;
+        int generic_image_margin = 100;
+        int position = argHolder.getAdapterPosition();
+
+        if (argSubscription.getLastItemUpdated() > 0 && argHolder.subTitle != null) {
+            String reportDate = DateUtils.getRelativeTimeSpanString(argSubscription.getLastItemUpdated()).toString();
+            String updatedAt = resources.getString(R.string.subscription_subtitle_updated_at);
+            argHolder.subTitle.setText(String.format("%s %s", updatedAt, reportDate));
         }
 
-        boolean hasImage = !TextUtils.isEmpty(logo);
-        Uri uri = hasImage ? Uri.parse(logo) : null;
-        int generic_image_margin = 100;
-
-        int visibility = hasNewEpisodes || !hasImage ? View.VISIBLE : View.GONE;
-
-        if (holder.new_episodes_counter != null && holder.new_episodes != null) {
+        if (argHolder.new_episodes_counter != null && argHolder.new_episodes != null) {
 
             if (hasNewEpisodes) {
-                holder.new_episodes_counter.setText(String.valueOf(newEpisodeCount));
-                holder.new_episodes.setText(pluralNew);
+                argHolder.new_episodes_counter.setText(String.valueOf(newEpisodeCount));
+                argHolder.new_episodes.setText(pluralNew);
             }
 
-            holder.new_episodes_counter.setVisibility(visibility);
-            holder.new_episodes.setVisibility(visibility);
+            argHolder.new_episodes_counter.setVisibility(visibility);
+            argHolder.new_episodes.setVisibility(visibility);
         } else {
             String title = String.valueOf(newEpisodeCount) + " " + pluralNew;
             if (hasNewEpisodes && !isListView() && hasImage) {
-                holder.title.setText(title);
+                argHolder.title.setText(title);
             }
         }
 
-        if (holder.text_container != null) {
-            if (holder.image != null && hasImage) {
+        if (argHolder.text_container != null) {
+            if (argHolder.image != null && hasImage) {
 
-                subscription.getColors(mActivity)
+                argSubscription.getColors(mActivity)
                         .subscribeOn(io.reactivex.schedulers.Schedulers.io())
                         .observeOn(io.reactivex.android.schedulers.AndroidSchedulers.mainThread())
                         .subscribe(new BaseSubscription.BasicColorExtractorObserver<ColorExtractor>() {
 
                             @Override
                             public void onSuccess(ColorExtractor value) {
-                                holder.text_container.setBackgroundColor(value.getPrimaryTint());
+                                argHolder.text_container.setBackgroundColor(value.getPrimaryTint());
                             }
                         });
             } else {
-                holder.text_container.setBackgroundColor(0x00000000);
+                argHolder.text_container.setBackgroundColor(0x00000000);
             }
         }
 
         if (hasImage) {
-            holder.image.setPadding(0, 0, 0, 0);
-            String image = uri.toString();
+            argHolder.image.setPadding(0, 0, 0, 0);
+            String image = imageURI.toString();
             if (!TextUtils.isEmpty(image) && StrUtils.isValidUrl(image)) {
 
                 if (getItemViewType(position) == GRID_TYPE) {
-                    ImageLoaderUtils.getGlide(mActivity, image).centerCrop().placeholder(R.drawable.generic_podcast).into(holder.image);
+                    ImageLoaderUtils.getGlide(mActivity, image).centerCrop().placeholder(R.drawable.generic_podcast).into(argHolder.image);
                 } else {
                     ImageLoaderUtils.getGlide(mActivity, image)
                             .centerCrop()
                             .placeholder(R.drawable.generic_podcast)
-                            .into(new BitmapImageViewTarget(holder.image) {
+                            .into(new BitmapImageViewTarget(argHolder.image) {
                                 @Override
                                 protected void setResource(Bitmap resource) {
                                     RoundedBitmapDrawable circularBitmapDrawable =
@@ -275,56 +200,67 @@ public class SubscriptionAdapter extends RecyclerView.Adapter {
 
                                     circularBitmapDrawable.setCornerRadius(radius);
 
-                                    holder.image.setImageDrawable(circularBitmapDrawable);
+                                    argHolder.image.setImageDrawable(circularBitmapDrawable);
                                 }
                             });
                 }
 
             }
         } else {
-            holder.image.setPadding(generic_image_margin, generic_image_margin, generic_image_margin, generic_image_margin);
-            Glide.with(mActivity).load(R.drawable.generic_podcast).centerCrop().into(holder.image);
+            argHolder.image.setPadding(generic_image_margin, generic_image_margin, generic_image_margin, generic_image_margin);
+            Glide.with(mActivity).load(R.drawable.generic_podcast).centerCrop().into(argHolder.image);
         }
 
-        if (holder.text_container != null) {
-            holder.text_container.setVisibility(visibility);
+        if (argHolder.text_container != null) {
+            argHolder.text_container.setVisibility(visibility);
         }
 
         if (isListView() || !hasImage) {
-            holder.title.setText(subscription.getTitle());
+            argHolder.title.setText(argSubscription.getTitle());
         }
 
 
         argHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!mMultiSelector.tapSelection(holder)){
+                if (!mMultiSelector.tapSelection(argHolder)){
                     // do whatever we want to do when not in selection mode
                     // perhaps navigate to a detail screen
-                    FeedActivity.start(mActivity, subscription);
+                    FeedActivity.start(mActivity, argSubscription);
                 } else {
-                    setActionModeTitle(mMultiSelector.getSelectedPositions().size());
+                    onSubscriptionSelected();
                 }
             }
         });
 
-        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+        argHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
 
-                AppCompatActivity appCompatActivity = ((AppCompatActivity) holder.itemView.getContext());
+                AppCompatActivity appCompatActivity = ((AppCompatActivity) argHolder.itemView.getContext());
                 mActionMode = appCompatActivity.startSupportActionMode(mActionModeCallback);
 
                 if (!mMultiSelector.isSelectable()) {
                     mMultiSelector.setSelectable(true);
-                    mMultiSelector.setSelected(holder, true);
+                    mMultiSelector.setSelected(argHolder, true);
                     return true;
                 }
-                return false;
+
+                mMultiSelector.tapSelection(argHolder);
+                onSubscriptionSelected();
+
+                return true;
 
             }
         });
+    }
 
+    private void onSubscriptionSelected() {
+        int numSelected = mMultiSelector.getSelectedPositions().size();
+        setActionModeTitle(numSelected);
+        if (numSelected < 1 && mActionMode != null) {
+            mActionMode.finish();
+        }
     }
 
     public void setDataset(@NonNull SortedList<Subscription> argSubscriptions) {
@@ -339,10 +275,7 @@ public class SubscriptionAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemViewType(int position) {
-        if (isLastItem(position))
-            return FOOTER_TYPE;
-
-        Subscription sub = null;
+        Subscription sub;
         try {
             sub = mSubscriptions.get(position);
         } catch (IllegalStateException e) {
@@ -355,10 +288,6 @@ public class SubscriptionAdapter extends RecyclerView.Adapter {
         }
 
         return numberOfColumns == 1 ? LIST_TYPE : GRID_TYPE;
-    }
-
-    private boolean isLastItem(int argPosition) {
-        return false; //argPosition +1 == getItemCount();
     }
 
     private boolean isListView() {
@@ -407,5 +336,69 @@ public class SubscriptionAdapter extends RecyclerView.Adapter {
         }
 
         return false;
+    }
+
+    private ModalMultiSelectorCallback getModalMultiSelectorCallback() {
+        return new ModalMultiSelectorCallback(mMultiSelector) {
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+
+                List<Integer> positions = mMultiSelector.getSelectedPositions();
+
+                if (mSubscriptions == null)
+                    return false;
+
+                switch (menuItem.getItemId()) {
+                    case R.id.unsubscribe:
+                        actionMode.finish();
+
+                        for (int i = 0; i < positions.size(); i++) {
+                            int position = positions.get(i);
+                            Subscription subscription = mSubscriptions.get(position);
+
+                            if (subscription == null)
+                                return false;
+
+                            subscription.unsubscribe("Unsubscribe:context");
+                            notifyItemRemoved(position);
+                        }
+
+                        mMultiSelector.clearSelections();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                super.onCreateActionMode(actionMode, menu);
+                mActivity.getMenuInflater().inflate(R.menu.subscription_context, menu);
+
+                return true;
+            }
+
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                boolean onPrepared = super.onPrepareActionMode(mode, menu);
+
+                String title = String.valueOf(mMultiSelector.getSelectedPositions().size() + 1 );
+                mode.setTitle(title);
+
+                return onPrepared;
+            }
+
+            public void onDestroyActionMode(ActionMode mode) {
+                super.onDestroyActionMode(mode);
+            }
+        };
+    }
+
+    private CharSequence getNewEpisodesString(int newEpisodeCount) {
+        CharSequence pluralNew = "";
+        if (newEpisodeCount > 0) {
+            pluralNew = mActivity.getResources().getQuantityText(R.plurals.subscription_list_new, newEpisodeCount);
+        }
+
+        return pluralNew;
     }
 }
