58ef5eba67e138215d865b683779299cac94b14c
Arvid Böttiger <bottiger@gmail.com>
2016-12-14 13:44:54 +0100

    Cleaned up the SubscriptionAdapter. Better handling of subscription multi sleection

--- app/src/main/java/org/bottiger/podcast/adapters/viewholders/subscription/AuthenticationViewHolder.java
+++ app/src/main/java/org/bottiger/podcast/adapters/viewholders/subscription/AuthenticationViewHolder.java
@@ -1,22 +1,35 @@
 package org.bottiger.podcast.adapters.viewholders.subscription;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.bignerdranch.android.multiselector.MultiSelector;
+
+import org.bottiger.podcast.R;
 import org.bottiger.podcast.views.dialogs.DialogFeedAuthentication;
 
 /**
  * Created by aplb on 31-12-2015.
  */
-public class AuthenticationViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class AuthenticationViewHolder extends BaseAnimatedSelectableViewHolder implements View.OnClickListener {
 
+    private View mContainer;
     public String url = null;
 
-    public AuthenticationViewHolder(View itemView) {
-        super(itemView);
+    public AuthenticationViewHolder(View itemView, MultiSelector argMultiSelector) {
+        super(itemView, argMultiSelector);
+
+        mContainer = itemView.findViewById(R.id.subscription_container);
 
         itemView.setOnClickListener(this);
+    }
+
+    @NonNull
+    @Override
+    View getContainerView() {
+        return mContainer;
     }
 
     @Override
