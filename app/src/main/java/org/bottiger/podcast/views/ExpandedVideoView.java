099674ad40d832ca242ac83d808a03d3d5036445
Arvid Böttiger <bottiger@gmail.com>
2016-12-19 00:07:52 +0100

    Enhanced the IntroFragment for KitKat (4.4)

---
+++ app/src/main/java/org/bottiger/podcast/views/ExpandedVideoView.java
@@ -0,0 +1,59 @@
+package org.bottiger.podcast.views;
+
+import android.content.Context;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+/**
+ * Created by aplb on 18-12-2016.
+ */
+
+public class ExpandedVideoView extends VideoView {
+
+    private int mVideoWidth;
+    private int mVideoHeight;
+
+    public ExpandedVideoView(Context context) {
+        super(context);
+    }
+
+    public ExpandedVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpandedVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public ExpandedVideoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void setVideoURI(Uri uri) {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(this.getContext(), uri);
+        mVideoWidth = Integer.parseInt(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH));
+        mVideoHeight = Integer.parseInt(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
+        super.setVideoURI(uri);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = getDefaultSize(mVideoWidth, widthMeasureSpec);
+        int height = getDefaultSize(mVideoHeight, heightMeasureSpec);
+        if (mVideoWidth > 0 && mVideoHeight > 0) {
+            if (mVideoWidth * height > width * mVideoHeight) {
+                height = width * mVideoHeight / mVideoWidth;
+            } else if (mVideoWidth * height < width * mVideoHeight) {
+                width = height * mVideoWidth / mVideoHeight;
+            }
+        }
+        setMeasuredDimension(width, height);
+    }
+}
